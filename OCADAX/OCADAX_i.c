de1899d7de6ddf673f6897f00ba45a04ed9cd211
lhf <LiarMaiq@163.com>
2016-11-21 21:02:09 +0800

    Add .gitignore

--- OCADAX/OCADAX_i.c
+++ OCADAX/OCADAX_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sat Oct 29 13:51:05 2016
+/* at Mon Nov 21 20:55:34 2016
  */
 /* Compiler settings for OCADAX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
